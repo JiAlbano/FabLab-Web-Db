64b084a2193d9972e68ba722be709ad1d868c991
Aaron Serrano <amserrano1337@gmail.com>
2022-10-19 15:30:52 +0800

    Modified login and signup forms for main_controller

--- rails/app/controllers/main_controller.rb
+++ rails/app/controllers/main_controller.rb
@@ -4,10 +4,42 @@ class MainController < ApplicationController
     end
 
     def signup
+        @customer = Customer.new(signup_login_params)
+        @customer_detail = CustomerDetail.new(signup_detail_params)
 
+        if @customer.save
+            @customer_detail.customer = @customer
+            if @customer_detail.save
+                puts "successful registration"
+            else
+                @customer.destroy
+                puts "failed on customer detail"
+            end
+        else
+            puts "failed on customer"
+        end
     end
 
     def login
+        @customer = Customer.find_by(username: login_params[:username])
 
+        if @customer.present? && @customer.authenticate(login_params[:password])
+            puts "successful login"
+        else
+            puts "not successful login"
+        end
+    end
+
+    private
+    def signup_detail_params
+        params.require(:customer_detail).permit(:first_name, :last_name, :email, :contact_number);
+    end
+
+    def signup_login_params
+        params.require(:customer).permit(:username, :password, :password_confirmation);
+    end
+
+    def login_params
+        params.require(:customer).permit(:username, :password);
     end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Login and Register
+  post "/login", to: "main#login"
+  post "/signup", to: "main#signup"
+
   # Defines the root path route ("/")
   root "main#index"
 
