60f34385422d7f4c0a0a402d69fc20e472da0630
Paul Vincent Llesol <pvllesol@gbox.adnu.edu.ph>
2022-10-24 14:10:40 +0800

    separated page for pending appointments and appointment history, adjustments to front-end for pending appointments and appointments history, added appointment request page with appointment request form.

--- rails/app/controllers/dashboard_controller.rb
+++ rails/app/controllers/dashboard_controller.rb
@@ -1,7 +1,15 @@
 class DashboardController < ApplicationController
   before_action :require_customer_logged_in!
 
-  def index
+  def pending
 
+  end
+
+  def history
+
+  end
+
+  def request_new
+    
   end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   get "/logout", to: "main#logout"
 
   # Dashboard related
-  get "/dashboard", to: "dashboard#index"
+  get "/request", to: "dashboard#request_new"
+  post "/request", to: "dashboard#request_new"
+  get "/pending", to: "dashboard#pending"
+  get "/history", to: "dashboard#history"
   get "/sidebar", to: "dashboard#sidebar"
   get "/request_appointment", to: "dashboard#request_appointment"
 
